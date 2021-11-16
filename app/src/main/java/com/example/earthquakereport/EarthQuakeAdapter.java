90dc30becadc4e429ddbc40f141f0eac4ecd9c50
zhavei <zhavei.vei@gmail.com>
2021-11-16 22:15:49 +0700

    add split strings.xml

--- app/src/main/java/com/example/earthquakereport/EarthQuakeAdapter.java
+++ app/src/main/java/com/example/earthquakereport/EarthQuakeAdapter.java
@@ -16,15 +16,18 @@ import java.util.Date;
 
 public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeModel> {
 
+    private static final String LOCATION_SEPARATOR = "of";
+
     public EarthQuakeAdapter( Context context, ArrayList<EarthQuakeModel> earthQuake) {
         super(context, 0, earthQuake);
     }
 
     private static class ViewHolder {
-        TextView textViewMag, textViewLocation, textViewDate, textViewTime;
+        TextView textViewMag, textViewLocation, textViewDate, textViewTime, textViewoffsetLocation;
 
         public ViewHolder(View convertView){
             textViewMag = convertView.findViewById(R.id.tv_magnitude);
+            textViewoffsetLocation = convertView.findViewById(R.id.tv_location_offset);
             textViewLocation = convertView.findViewById(R.id.tv_location);
             textViewDate = convertView.findViewById(R.id.tv_date);
             textViewTime = convertView.findViewById(R.id.tv_time);
@@ -39,7 +42,11 @@ public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeModel> {
         View itemView = convertView;
         ViewHolder viewHolder;
 
+        //indentify for split string
+        String primaryLocation;
+        String locationOffset;
 
+        //viewHolder Patern
         if (itemView == null) {
             itemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_layout, parent, false);
             viewHolder = new ViewHolder(itemView);
@@ -48,9 +55,11 @@ public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeModel> {
             viewHolder = (ViewHolder) itemView.getTag();
         }
 
+        //inflate to textview
         viewHolder.textViewMag.setText(currentPosition.getMagnitude());
         viewHolder.textViewLocation.setText(currentPosition.getLocation());
 
+        //get dateobject from position
         Date dateObject = new Date(currentPosition.getLongTime());
 
         String formattedDate = formatDate(dateObject);
@@ -61,6 +70,21 @@ public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeModel> {
         String formattedTime = formatTime(dateObject);
         // Display the time of the current earthquake in that TextView
         viewHolder.textViewTime.setText(formattedTime);
+
+        //split string
+        String originalLocation = currentPosition.getLocation();
+        if (originalLocation.contains(LOCATION_SEPARATOR)){
+            String [] parts = originalLocation.split(LOCATION_SEPARATOR);
+            locationOffset = parts[0] + LOCATION_SEPARATOR;
+            primaryLocation = parts[1];
+        } else {
+            locationOffset = getContext().getString(R.string.near_The);
+            primaryLocation = originalLocation;
+        }
+
+        //inflate to location IDs textview
+        viewHolder.textViewoffsetLocation.setText(locationOffset);
+        viewHolder.textViewLocation.setText(primaryLocation);
 
         return itemView;
     }
--- app/src/main/java/com/example/earthquakereport/QueryUtils.java
+++ app/src/main/java/com/example/earthquakereport/QueryUtils.java
@@ -56,10 +56,10 @@ public final class QueryUtils {
 
                  String magnitude = properties.getString("mag");
                  String location = properties.getString("place");
-                 String time = properties.getString("time");
+                 String date = properties.getString("time");
                  Long longTime = properties.getLong("time");
 
-                 EarthQuakeModel earthQuakeModel = new EarthQuakeModel(magnitude, location, time, longTime);
+                 EarthQuakeModel earthQuakeModel = new EarthQuakeModel(magnitude, location, date, longTime);
                  earthquakes.add(earthQuakeModel);
 
             }
